80ed98f9b0887f73d02296fe83c3baba2d5dcb29
Shiloh <yangxuru870918@gmail.com>
2023-05-25 17:39:41 +0800

    1. frontend: Finish login and register UI.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import BannerImage from "../assets/home-banner-image.png";
 import { FiArrowRight } from "react-icons/fi";
+import { Register } from "../components/Register";
 
 export default function Home() {
     return (
@@ -10,12 +11,14 @@ export default function Home() {
             <div className="home-banner-container">
                 <div className="home-text-section">
                     <h1 className="primary-heading">
-                      以想法為中心，
-                      <br/>
-                      盡情探究
+                        以想法為中心，
+                        <br/>
+                        盡情探究
                     </h1>
                     <button className="secondary-button">
-                      加入我們吧！ <FiArrowRight />{" "}
+                        加入我們吧！前往
+                        <Register />
+                        <FiArrowRight />{" "}
                     </button>
                 </div>
                 <div className="home-image-section">
