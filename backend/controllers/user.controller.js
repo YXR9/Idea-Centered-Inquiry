4e97d06f2ce8fed6c092fe3178f6318bbbab7ca4
YXR9 <yangxuru870918@gmail.com>
2023-06-19 21:51:40 +0800

    env(package): version of JWT

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -55,30 +55,32 @@ exports.login = async (req, res) => {
     });
 
     //if user email is found, compare password with bcrypt
-    if (user) {
-      const isSame = await bcrypt.compare(password, user.password);
-
-      //if password is the same
-       //generate token with the user's id and the secretKey in the env file
-
-      if (isSame) {
-        let token = jwt.sign({ id: user.id }, process.env.secretKey, {
-          expiresIn: 1 * 24 * 60 * 60 * 1000,
-        });
-
-        //if password matches wit the one in the database
-        //go ahead and generate a cookie for the user
-        res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
-        console.log("user", JSON.stringify(user, null, 2));
-        console.log(token);
-        //send user data
-        return res.status(201).send(user);
-      } else {
-        return res.status(401).send("Authentication failed");
-      }
-    } else {
+    if (!user) {
       return res.status(401).send("Authentication failed");
     }
+
+    const isSame = await bcrypt.compare(password, user.password);
+    console.log('1')
+    //if password is the same
+      //generate token with the user's id and the secretKey in the env file
+
+    if (!isSame) {
+      return res.status(401).send("Authentication failed");
+    }
+    console.log('2')
+    let token = jwt.sign({ id: user.id }, process.env.secretKey, {
+      expiresIn: 1 * 24 * 60 * 60 * 1000,
+    });
+
+    console.log('3')
+    //if password matches wit the one in the database
+    //go ahead and generate a cookie for the user
+    res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
+    console.log("user", JSON.stringify(user, null, 2));
+    console.log(token);
+    //send user data
+    return res.status(201).send(user);
+
   } catch (error) {
     console.log(error);
   }
