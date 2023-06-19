3aada6200ab6b92ccb92a4c4339c1a37bf9d78d1
YXR9 <yangxuru870918@gmail.com>
2023-06-19 20:40:27 +0800

    fix(backend): Config of DB

--- backend/models/index.js
+++ backend/models/index.js
@@ -4,7 +4,7 @@ const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
-  operatorsAliases: false,
+  operatorsAliases: 0,
 
   pool: {
     max: dbConfig.pool.max,
