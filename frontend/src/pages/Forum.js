3982834a23090a1930dc8cb3a517d3cd5bcacbd7
Shiloh <yangxuru870918@gmail.com>
2023-11-07 17:00:07 +0800

    Add(frontend): Add activity's forum's note style.

--- frontend/src/pages/Forum.js
+++ frontend/src/pages/Forum.js
@@ -3,13 +3,14 @@ import ForumPage_Navbar from '../components/ForumPage_Navbar';
 import Graph from 'react-vis-network-graph';
 import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import NoteIcon from '../assets/sticky-note.png';
 // import {visNetworkOptions as option} from '../../utils/VisNetworkOptions';
 
 export default function Forum() {
   const graph = {
     nodes: [
-      { id: 1, label: "node 1", title: 'this is title 1', shape: "box" },
-      { id: 2, label: "node 2", title: 'this is title 2', shape: "box" }
+      { id: 1, label: "node 1", title: 'this is title 1', shape: "image", image: NoteIcon, size: 100 },
+      { id: 2, label: "node 2", title: 'this is title 2', shape: "image", image: NoteIcon, size: 100  }
     ],
     edges: [
       { from: 1, to: 2 }
@@ -26,8 +27,8 @@ export default function Forum() {
     nodes: {
       borderWidth: 0,
       borderWidthSelected: 0,
-      color: "#0262C4",
-      shape: "square",
+      // color: "#0262C4",
+      // shape: "square",
       size: 1,
       shadow: {
         enabled: true,
@@ -41,6 +42,22 @@ export default function Forum() {
         size: 13,
         bold: {
           mod: "bold"
+        }
+      }
+    },
+    manipulation: {
+      controlNodeStyle: {
+        shape: "dot",
+        size: 6,
+        color: {
+          background: "#ff0000",
+          border: "#3c3c3c",
+          highlight: {
+            background: "#07f968",
+            border: "#3c3c3c"
+          },
+          borderWidth: 2,
+          borderWidthSelected: 2
         }
       }
     }
