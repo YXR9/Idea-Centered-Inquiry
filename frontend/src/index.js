8c4b9bf3da569aedc50e432992a111927387c4b5
YXR9 <yangxuru870918@gmail.com>
2023-11-22 23:23:55 +0800

    Update(frontend): Removed React.StrictMode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import { AuthProvider } from "react-auth-kit";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
       <AuthProvider
         authType={"cookie"}
         authName={"_auth"}
@@ -14,5 +13,4 @@ root.render(
       >
           <App />
       </AuthProvider> 
-  </React.StrictMode>
 );
